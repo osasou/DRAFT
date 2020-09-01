3a266bf3263ca61959c654c876a445bfd0ec32a4
osasou <osasou0125@gmail.com>
2020-09-01 21:59:19 +0900

    add DRAFT_0_10

--- DRAFT_0_10/hard_clustering.m
+++ DRAFT_0_10/hard_clustering.m
@@ -1,12 +1,4 @@
-function hard_clustering()
-
-rng('default')  % For reproducibility
-mu1 = [1 2];
-sigma1 = [3 .2; .2 2];
-mu2 = [-1 -5];
-sigma2 = [2 0; 0 1];
-X = [mvnrnd(mu1,sigma1,200); mvnrnd(mu2,sigma2,100); mvnrnd(mu1,sigma1,200)];
-n = size(X,1);
+function [cluster1, cluster2, cluster3, cluster4, cluster5] = hard_clustering(X, user_num)
 
 figure
 scatter(X(:,1),X(:,2),10,'ko') % 10 means plot size, k means black, o means 'o' plot shape
@@ -14,20 +6,24 @@ scatter(X(:,1),X(:,2),10,'ko') % 10 means plot size, k means black, o means 'o' 
 % options = statset('Display','final'); 
 % gm = fitgmdist(X,2,'Options',options)
 
-gm = fitgmdist(X,2,'Start','plus') % Start, plus means useing K means++ Algo at first
+gm = fitgmdist(X,user_num,'Start','plus') % Start, plus means useing K means++ Algo at first
 
 hold on
 gmPDF = @(x,y)reshape(pdf(gm,[x(:) y(:)]),size(x)); % GMM(混合ガウスモデル) の確率密度関数 (pdf) , reshape(A,[2,3]) は A を 2 行 3 列の行列に形状変更
-fcontour(gmPDF,[-8 6]) % 関数 gmPDF = f(x,y) の等高線をプロット
+g = gca;
+fcontour(gmPDF,[g.XLim g.YLim]) % 関数 gmPDF(x,y) の等高線をプロット
 title('Scatter Plot and Fitted GMM Contour')
 hold off
 
 idx = cluster(gm,X);
 cluster1 = (idx == 1); % |1| for cluster 1 membership
 cluster2 = (idx == 2); % |2| for cluster 2 membership
+cluster3 = (idx == 3);
+cluster4 = (idx == 4);
+cluster5 = (idx == 5);
 
 figure
-gscatter(X(:,1),X(:,2),idx,'rb','+o')
-legend('Cluster 1','Cluster 2','Location','best')
+gscatter(X(:,1),X(:,2),idx,'rgbkm','+osd*')
+legend('Cluster 1','Cluster 2','Cluster 3','Cluster 4','Cluster 5','Location','best')
 
 end
